7d8a9ed78baedb098ca0a4d13123a57a1b8e2fd5
Astitva Singh <singhastitva99@gmail.com>
2023-03-13 01:48:46 +0530

    initial commit

---
+++ app.js
@@ -0,0 +1,48 @@
+const express = require('express');
+const nodemailer = require('nodemailer');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const path = require('path');
+
+const app = express();
+app.use(cors());
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname + "/public")))
+
+app.get('/form', (req, res) => {
+    res.send('Hello World!');
+});
+
+
+app.post("/form", (req, res) => {
+    const { name, email, age, number } = req.body;
+
+    const transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: 'singhastitva99@gmail.com',
+            pass: 'cdiwjjbpkthudrtf',
+        },
+    });
+
+    const mailOptions = {
+        from: 'singhastitva99@gmail.com',
+        to: email,
+        subject: 'StackFusion Assignment',
+        text: `YOUR NAME IS: ${name} YOUR EMAIL IS: ${email} YOUR DOB IS: ${age} YOUR NUMBER IS: ${number}`,
+    };
+
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            console.error(error);
+            res.status(500).send('Could not send email.');
+        } else {
+            console.log(`Email sent: ${info.response}`);
+            res.status(200).send('Email sent successfully!');
+        }
+    });
+});
+
+app.listen(3000, () => {
+    console.log('Hi astitva, server started on port 3000');
+});
